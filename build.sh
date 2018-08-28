b1abe3e3bb0410c31389c86e5f77770ff8023a0c
Grzegorz Klimek <kfiku.com@gmail.com>
2018-08-28 19:05:13 +0200

    add build container

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 docker build -t kfiku/docker-php-nginx-cron .
+docker build -t kfiku/docker-php-nginx-cron:build build
--- publish.sh
+++ publish.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 docker push kfiku/docker-php-nginx-cron:latest
+docker push kfiku/docker-php-nginx-cron:build

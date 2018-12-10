8a298b6f474d7ce18190833dbbf2c5db7f3d7cf7
grzegorz <grzegorz@e-gaming.cz>
2018-12-10 12:39:06 +0100

    fix formating

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "composer version:" && docker run --rm kfiku/docker-php-nginx-cron:build composer --version
-echo "php version:"  && docker run --rm kfiku/docker-php-nginx-cron:build php --version
-echo "node version:" && docker run --rm kfiku/docker-php-nginx-cron:build node --version
-echo "npm version:"  && docker run --rm kfiku/docker-php-nginx-cron:build npm --version
+echo "php version:"      && docker run --rm kfiku/docker-php-nginx-cron:build php --version
+echo "node version:"     && docker run --rm kfiku/docker-php-nginx-cron:build node --version
+echo "npm version:"      && docker run --rm kfiku/docker-php-nginx-cron:build npm --version
 
